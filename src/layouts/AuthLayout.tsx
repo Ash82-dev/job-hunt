ede2adaef862c2778fc922d8fabb4cb0a9869581
Amir Shariati <amirshrt1382@gmail.com>
2026-07-19 19:11:18 +0330

    feat: implement register UI

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router";
 
 function AuthLayout() {
-  return <Outlet />;
+  return (
+    <main className="bg-background w-screen h-screen flex items-center px-4 justify-center">
+      <div className="max-w-lg grow rounded-md shadow-xl bg-surface px-6 py-8">
+        <Outlet />
+      </div>
+    </main>
+  );
 }
 
 export default AuthLayout;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,51 @@
 import { Link } from "react-router";
+
 import { routes } from "../router/routes";
+import Button from "../components/Button";
+import Input from "../components/Input";
 
 function Register() {
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to={routes.login}>Login</Link>
-      <Link to={routes.forgetPassword}>Forget Password</Link>
+    <div className="w-full">
+      <form
+        onSubmit={(event) => handleSubmit(event)}
+        className="flex flex-col gap-15 items-center"
+      >
+        <h1 className="text-3xl text-primary font-semibold">
+          Welcome to Job Hunt
+        </h1>
+
+        <div className="w-full space-y-4">
+          <Input id="email" label="Email" placeholder="Your email" />
+          <Input
+            id="password"
+            label="Password"
+            placeholder="Your password"
+            type="password"
+          />
+          <Link
+            to={routes.forgetPassword}
+            className="hover:underline hover:text-primary"
+          >
+            Have you forgot your Password?
+          </Link>
+        </div>
+
+        <div className="w-full flex flex-col gap-4 items-center">
+          <Button>Register</Button>
+
+          <Link
+            to={routes.login}
+            className="hover:underline hover:text-primary"
+          >
+            Already have an account? Login
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
