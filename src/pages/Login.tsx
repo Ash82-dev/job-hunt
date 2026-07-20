5b734cf7bff5b0f0dcd7616736e8948d52e6902a
Amir Shariati <amirshrt1382@gmail.com>
2026-07-20 10:36:41 +0330

    feat: implement login UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,51 @@
 import { Link } from "react-router";
 import { routes } from "../router/routes";
+import Button from "../components/Button";
+import Input from "../components/Input";
 
 function Login() {
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to={routes.register}>Register</Link>
-      <Link to={routes.forgetPassword}>Forget Password</Link>
+    <div className="w-full">
+      <form
+        onSubmit={(event) => handleSubmit(event)}
+        className="flex flex-col gap-15 items-center"
+      >
+        <h1 className="text-3xl text-primary font-semibold">
+          Welcome back to Job Hunt
+        </h1>
+
+        <div className="w-full space-y-4">
+          <Input id="email" label="Email" placeholder="Your email" />
+          <Input
+            id="password"
+            label="Password"
+            placeholder="Your password"
+            type="password"
+          />
+          <Link
+            to={routes.forgetPassword}
+            className="hover:underline hover:text-primary"
+          >
+            Have you forgot your Password?
+          </Link>
+        </div>
+
+        <div className="w-full flex flex-col gap-4 items-center">
+          <Button>Login</Button>
+
+          <Link
+            to={routes.register}
+            className="hover:underline hover:text-primary"
+          >
+            Don't have an account?{" "}
+            <span className="text-primary">Register now</span>
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
