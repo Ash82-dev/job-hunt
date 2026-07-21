edee3f43c43bf25c5042ea701690f9289d16ce82
Amir Shariati <amirshrt1382@gmail.com>
2026-07-21 11:52:32 +0330

    feat: add UI for creating application item

--- src/features/dashboard/components/ApplicationItem.tsx
+++ src/features/dashboard/components/ApplicationItem.tsx
@@ -1,4 +1,23 @@
-function ApplicationItem() {
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+
+type ApplicationItemProps = {
+  mode?: "creating" | "item";
+};
+
+function ApplicationItem({ mode = "item" }: ApplicationItemProps) {
+  if (mode === "creating") {
+    return (
+      <li className="bg-surface px-3 py-4 rounded-md shadow-md text-on-surface flex flex-col gap-4">
+        <Input id="title" placeholder="Application title" />
+        <div className="flex gap-2 justify-end w-1/2 ml-auto">
+          <Button>Add</Button>
+          <Button variant="error">Cancel</Button>
+        </div>
+      </li>
+    );
+  }
+
   return (
     <li className="bg-surface px-2 py-4 rounded-md shadow-md text-on-surface">
       <h3>TITLE</h3>
