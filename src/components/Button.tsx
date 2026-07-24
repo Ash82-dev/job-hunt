80993a0cff2cce38d33ede15220f32e44e7ee6df
Amir Shariati <amirshrt1382@gmail.com>
2026-07-24 22:05:28 +0330

    fix: use error color token for button error variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 const styles = {
   base: "enabled:hover:brightness-70 font-semibold disabled:brightness-50 disabled:cursor-not-allowed rounded-sm enabled:cursor-pointer flex items-center justify-center",
   primary: "bg-primary text-on-primary",
-  error: "bg-tertiary text-on-tertiary",
+  error: "bg-error text-on-error",
 };
 
 const sizeStyles = {
