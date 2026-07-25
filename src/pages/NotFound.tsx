f3c4fcd3e0fa44323462263c19c4c27f1b5764b8
Amir Shariati <amirshrt1382@gmail.com>
2026-07-25 11:36:29 +0330

    feat: implement not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,20 @@
+import Button from "@/components/Button";
+import { routes } from "@/router/routes";
+import { useNavigate } from "react-router";
+
 function NotFound() {
-  return <div>Page Not Found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <main className="w-dvw h-dvh flex flex-col gap-20 bg-surface items-center justify-center">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold text-on-surface">
+        404 Page not found
+      </h1>
+      <Button className="px-10 max-w-70" onClick={() => navigate(routes.login)}>
+        Back to login
+      </Button>
+    </main>
+  );
 }
 
 export default NotFound;
